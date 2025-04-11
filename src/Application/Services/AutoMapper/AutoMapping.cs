fb10b4f873b6894399a82f44c7673d5447a91ffb
Edson Gomes <biduzao.bidu21@gmail.com>
2025-04-11 04:32:39 -0300

    feat: add automapper

---
+++ src/Application/Services/AutoMapper/AutoMapping.cs
@@ -0,0 +1,20 @@
+// Importações
+using AutoMapper; // Biblioteca responsável pelo mapeamento entre classes
+using Application.DTOs.Requests; // Classe RegistraEquipamentoRequest
+using Domain.Entities; // Entidade Equipamento
+
+namespace Application.Services.AutoMapper;
+
+// Classe AutoMapping herda de Profile, que é uma classe base do AutoMapper
+// Essa herança é necessária para configurar os mapeamentos
+public class AutoMapping : Profile
+{
+    // Construtor da classe AutoMapping
+    // O AutoMapper irá chamar esse construtor para registrar os mapeamentos
+    public AutoMapping()
+    {
+        // Aqui fazemos o mapeamento de RegistraEquipamentoRequest para Equipamento
+        // Isso diz ao AutoMapper como transformar um DTO de request em uma entidade
+        CreateMap<RegistraEquipamentoRequest, Equipamento>();
+    }
+}

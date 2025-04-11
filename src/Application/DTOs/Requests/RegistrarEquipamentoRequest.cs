46b92969308bfc034b86a125e375310f1c1add5e
Edson Gomes <biduzao.bidu21@gmail.com>
2025-04-11 04:36:51 -0300

    feat: add euipamento register request DTO

---
+++ src/Application/DTOs/Requests/RegistrarEquipamentoRequest.cs
@@ -0,0 +1,6 @@
+namespace Application.DTOs.Requests;
+public class RegistraEquipamentoRequest
+{
+    public string Nome {get; set;} = string.Empty;
+    public string Descricao {get; set;} = string.Empty;
+}

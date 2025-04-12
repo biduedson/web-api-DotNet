829736cd158625031e48f6bddd416b6166334b13
Edson Gomes <biduzao.bidu21@gmail.com>
2025-04-11 21:43:04 -0300

    feat: add services Application and Infrastructure

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,10 +1,6 @@
-using Application.UseCases.CriarEquipamento;
 using Domain.Repositories.EquipamentoRepository;
-using Infrastructure.Data.Context;
+using Infrastructure;
 using Infrastructure.Data.Repositories;
-
-
-using Microsoft.EntityFrameworkCore;
 using Application;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,18 +10,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AdicionarApplication(builder.Configuration);
+builder.Services.AdicionarInfrastructure(builder.Configuration);
 
-// DB
-
-var connectionString = builder.Configuration.GetConnectionString("Default");
-builder.Services.AddDbContext<AppDbContext>(options =>
-options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
-
-
-//inm=jeçao de dedpendencia
 
 builder.Services.AddScoped<IEquipamentoRepository, EquipamentoRepository>();
-builder.Services.AddScoped<CriarEquipamentoUseCase>();
+
 
 var app = builder.Build();
 

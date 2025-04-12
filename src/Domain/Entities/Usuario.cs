55914b1694c0eb9f0a4c74230b34f2ecf941cc85
Edson Gomes <biduzao.bidu21@gmail.com>
2025-04-11 21:47:11 -0300

    feat: add entity Usuario

---
+++ src/Domain/Entities/Usuario.cs
@@ -0,0 +1,13 @@
+namespace Domain.Entities;
+
+public class Usuario 
+{
+    public Guid Id {get; set; }
+    public string Nome {get;set;} = string.Empty;
+    public string Email {get; set;} = string.Empty;
+    public string SenhaHash {get; set;} = string.Empty;
+    public bool IsAdmin {get; set;} = false;
+
+
+    public void TornarUsuarioAdmin() => IsAdmin = true;
+}

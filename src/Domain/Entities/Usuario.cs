7dc223e60038b7ec7885f470a6d741c41e3af19b
Edson Gomes <biduzao.bidu21@gmail.com>
2025-04-14 20:30:44 -0300

    feat: alter entity usuario

--- src/Domain/Entities/Usuario.cs
+++ src/Domain/Entities/Usuario.cs
@@ -5,9 +5,11 @@ public class Usuario
     public Guid Id {get; set; }
     public string Nome {get;set;} = string.Empty;
     public string Email {get; set;} = string.Empty;
-    public string Senha {get; set;} = string.Empty;
-    public bool IsAdmin {get; set;} = false;
+    public string Senha {get; set;} = string.Empty; 
+    public bool Ativo { get; set; } = true;
+    public DateTime DataCriacao { get; set; } = DateTime.UtcNow;
+    public bool Administrador {get; set;} = false;
 
 
-    public void TornarUsuarioAdmin() => IsAdmin = true;
+    public void TornarUsuarioAdmin() => Administrador = true;
 }

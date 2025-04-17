f5f385861b2254672a2b06ea949564b0f4d361b6
Edson Gomes <biduzao.bidu21@gmail.com>
2025-04-17 19:37:42 -0300

    feat: add entity reservadeequiopamento

---
+++ src/Domain/Entities/ReservaDeEquipamento.cs
@@ -0,0 +1,14 @@
+using Domain.Enums;
+
+namespace Domain.Entities;
+
+public class ReservaDeEquipamento
+{
+    public Guid Id {get; set;}
+    public Guid UsuarioId {get; set;}
+    public Guid EquipamentoId {get; set;}
+    public DateTime DataDaReserva {get; set;} = DateTime.UtcNow;
+    public StatusReserva Status {get; set;}
+    public Usuario Usuario {get; set;} = null!;
+    public Equipamento Equipamento {get; set;} = null!;
+}

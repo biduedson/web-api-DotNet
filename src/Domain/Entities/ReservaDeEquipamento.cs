5c0af39d384248e75162f293f5caf777270b2759
Edson Gomes <biduzao.bidu21@gmail.com>
2025-04-17 21:55:22 -0300

    refactor: DateTime.UtcNow removed

--- src/Domain/Entities/ReservaDeEquipamento.cs
+++ src/Domain/Entities/ReservaDeEquipamento.cs
@@ -7,7 +7,7 @@ public class ReservaDeEquipamento
     public Guid Id {get; set;}
     public Guid UsuarioId {get; set;}
     public Guid EquipamentoId {get; set;}
-    public DateTime DataDaReserva {get; set;} = DateTime.UtcNow;
+    public DateTime DataDaReserva {get; set;} 
     public StatusReserva Status {get; set;}
     public Usuario Usuario {get; set;} = null!;
     public Equipamento Equipamento {get; set;} = null!;
